31a73591089417922d3a12c518e2d1cea1dc0fab
Jhonatt <jhonattlima@gmail.com>
2019-10-07 23:14:43 -0300

    commit before refactoring everything in one screen

--- Assets/Scripts/Online/OnlineOrquestrator.cs
+++ Assets/Scripts/Online/OnlineOrquestrator.cs
@@ -5,53 +5,53 @@ using UnityEngine.Networking;
 
 public static class OnlineOrquestrator
 {
-    // VARIABLES
-    // List of players (max 2)
-    public static List<OnlinePlayerController> _playerList = new List<OnlinePlayerController>(); // ONLY SERVER WILL USE THIS
-    public static int lastTurn; // LAST TURN THAT MUST BE SYNCED IN ALL
+    // // VARIABLES
+    // // List of players (max 2)
+    // public static List<OnlinePlayerController> _playerList = new List<OnlinePlayerController>(); // ONLY SERVER WILL USE THIS
+    // public static int lastTurn; // LAST TURN THAT MUST BE SYNCED IN ALL
 
-    public static void addPlayer(OnlinePlayerController player){ // ONLY SERVER CALL THIS
-        _playerList.Add(player);
-        Debug.Log("OnlineOrquestrator.addPlayer: New player added to Orquestrator.");
-        Debug.Log("number odf players now: " + _playerList.Count);
-    }
+    // public static void addPlayer(OnlinePlayerController player){ // ONLY SERVER CALL THIS
+    //     _playerList.Add(player);
+    //     Debug.Log("OnlineOrquestrator.addPlayer: New player added, number os players now: " + _playerList.Count);
+    // }
 
-    public static void startGame(int getlastTurn, bool isServer){
-        // Random turn
-        lastTurn = getlastTurn;
-        // Start the game
-        SceneController.instance.changeScene(GameManager.instance.sceneBoard);
-        if(isServer){
-            foreach(var player in _playerList){
-                player.updateTurn(getlastTurn);
-            }
-        }
-    }
+    // public static void startGame(int getlastTurn, bool isServer){
+    //     // Random turn
+    //     lastTurn = getlastTurn;
+    //     // Start the game
+    //     //Debug.LogError("Reached start game");
+    //     SceneController.instance.changeScene(GameManager.instance.sceneBoard);
+    //     if(isServer){
+    //         foreach(var player in _playerList){
+    //             player.startGame(getlastTurn);
+    //             player.changeTurn(getlastTurn);
+    //         }
+    //     }
+    // }
 
-    public static void changeTurn(){
-        if(lastTurn == 1){
-            lastTurn = 2;
-        } 
-        else{
-            lastTurn = 1;
-        } 
-        Debug.Log("Last turn: Player " + lastTurn);
-        // Change Board panel text
-        BoardManager.instance.textTurn.text =  "Turn: Player " + lastTurn;
-    }
+    // public static void changeTurn(int turn, bool isServer){
+    //     if(turn == 1) lastTurn = 2 ;
+    //     else lastTurn = 1;
+    //     Debug.LogError("Last turn: Player " + lastTurn);
+    //     // Change Board panel text
+    //     BoardManager.instance.textTurn.text =  "Turn: Player " + lastTurn;
+    //     if(isServer){
+    //         foreach(var player in _playerList){
+    //             player.changeTurn(lastTurn);
+    //         }
+    //     }
+    // }
 
-    public static void setPlay(int xpos, int ypos, int playerMagic, bool isServer){
-        BoardManager.instance.setMagic(xpos, ypos, playerMagic);
-        changeTurn();
-        if(isServer){
-            foreach(var player in _playerList){
-                player.castMagic(xpos, ypos, playerMagic, lastTurn);
-                player.updateTurn(lastTurn);
-            }
-        }
-    }
+    // public static void setPlay(int xpos, int ypos, int playerMagic, bool isServer){ 
+    //     BoardManager.instance.setMagic(xpos, ypos, playerMagic);
+    //     if(isServer){
+    //         foreach(var player in _playerList){
+    //             player.castMagic(xpos, ypos, playerMagic);
+    //         }
+    //     }
+    // }
 
-    public static void endGame(){
-        // Close connections, destroy objects and return to lobby.
-    }
+    // public static void endGame(){
+    //     // Close connections, destroy objects and return to lobby.
+    // }
 }
--- Assets/Scripts/Online/OnlinePlayerController.cs
+++ Assets/Scripts/Online/OnlinePlayerController.cs
@@ -17,83 +17,131 @@ public class OnlinePlayerController : NetworkBehaviour
     }
 
     void Start(){
-        if(isServer && !isLocalPlayer || !isServer && isLocalPlayer){ // Check if is the player 2, on client's only machine (need to pass this info to server)
-            this.playerMagic = 2;
-            this.playerNumber = 2;
-        } else if (isServer && isLocalPlayer) {
+        if ((isServer && isLocalPlayer) || (!isServer && !isLocalPlayer))
+        {
             this.playerMagic = 1;
             this.playerNumber = 1;
         }
-        if(isServer){
-            OnlineOrquestrator.addPlayer(this); // Only server will have the list of players
+        else if((!isServer && isLocalPlayer) || (isServer && !isLocalPlayer))
+        { // Check if is the player 2, on client's only machine (need to pass this info to server)
+            this.playerMagic = 2;
+            this.playerNumber = 2;
+            
         }
-        if(isLocalPlayer && !isServer){
-            CmdStartGame(Random.Range(1,2));
+        if(!isServer && isLocalPlayer){
+            CmdStartGame(Random.Range(1,3));  // START THE GAME WITH RANDOM PLAYER  
         }
+    }
+
+    [Command]
+    private void CmdStartGame(int turn)
+    {
+        Debug.LogError("Tried to start game using Command with player number " + this.netId);
+        RpcStartGame(turn);
+    }
+
+    [ClientRpc]
+    private void RpcStartGame(int turn)
+    {
+        if(!isLocalPlayer) return;
+        Debug.LogError("Tried to start the game using RPC with player number " + netId);
+        lastPlayed = turn;
+        SceneController.instance.changeScene(GameManager.instance.sceneBoard);
+    }
+
+    [Command]
+    private void CmdCastMagic(int xpos, int ypos, int playerMagic)
+    {
+        RpcCastMagic(xpos, ypos, playerMagic);
+    }
+
+    [ClientRpc]
+    private void RpcCastMagic(int xpos, int ypos, int playerMagic)
+    {
+        if(!isLocalPlayer) return;
+        BoardManager.instance.setMagic(xpos, ypos, playerMagic);
+        changeTurn();
+        played = false;
+    }
+
+    private void changeTurn()
+    {
+        if(lastPlayed == 1) lastPlayed = 2;
+        else lastPlayed = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         if(!isLocalPlayer) return;
-        if((isServer && lastPlayed == 2) || (!isServer && lastPlayed == 1)){
-            if(BoardManager.instance.getHit()){
-                played = true;
-                CmdCastMagic(BoardManager.instance.lastMovementSet[0], BoardManager.instance.lastMovementSet[1], playerMagic);
+        if((isServer && lastPlayed == 2) || (!isServer && lastPlayed == 1))
+        {
+            if(BoardManager.instance != null)
+            {
+                if(BoardManager.instance.getHit())
+                {
+                    played = true;
+                    CmdCastMagic(BoardManager.instance.lastMovementSet[0], BoardManager.instance.lastMovementSet[1], playerMagic);
+                }
             }
         }
     }
 
-    public void startGame(int turn){
-        RpcStartGame(turn);
-    } 
+    // public void startGame(int turn){ // Called only in server
+    //     RpcStartGame(turn);
+    // } 
 
-    public void updateTurn(int turn){
-        RpcUpdateTurn(turn);
-    }
+    // [Command]
+    // private void CmdStartGame(int turn){
+    //     OnlineOrquestrator.startGame(turn, true);
+    // }
 
-    [ClientRpc]
-    private void RpcUpdateTurn(int turn){
-        if(!isLocalPlayer) return;
-        this.lastPlayed = turn;
-    }
+    // [ClientRpc]
+    // private void RpcStartGame(int turn){
+    //     if(!isLocalPlayer) return;
+    //     OnlineOrquestrator.startGame(turn, false);
+    // }
 
-    [Command]
-    private void CmdStartGame(int turn){
-        OnlineOrquestrator.startGame(turn, true);
-    }
+    // public void changeTurn(int turn){ // Called only in server
+    //     RpcChangeTurn(turn);
+    // }
 
-    [ClientRpc]
-    private void RpcStartGame(int turn){
-        if(!isLocalPlayer) return;
-        OnlineOrquestrator.startGame(turn, false);
-    } 
+    // [Command]
+    // public void CmdChangeTurn(int turn){
+    //     OnlineOrquestrator.changeTurn(turn, true);
+    // } 
 
-    // Method only called by OnlineOrquestrator in server
-    public void castMagic(int xpos, int ypos, int playerMagic, int lastPlayed){
-        RpcUpdateBoard(xpos, ypos, playerMagic, lastPlayed);
-    }
+    // [ClientRpc]
+    // private void RpcChangeTurn(int turn){
+    //     if(!isLocalPlayer) return;
+    //     this.lastPlayed = turn;
+    // }
 
-    [Command]
-    private void CmdCastMagic(int xpos, int ypos, int playerMagic){
-        OnlineOrquestrator.setPlay(xpos, ypos, playerMagic, true);
-    }
+    // public void castMagic(int xpos, int ypos, int playerMagic){ // Called only in server
+    //     RpcUpdateBoard(xpos, ypos, playerMagic);
+    // }
 
-    [ClientRpc]
-    private void RpcUpdateBoard(int xpos, int ypos, int playerMagic, int lastPlayed){
-        if(!isLocalPlayer) return;
-        OnlineOrquestrator.setPlay(xpos, ypos, lastPlayed, false);
-        //BoardManager.instance.setMagic(i,j,playerMagic);
-        // int result = BoardManager.instance.isGameOver(BoardManager.instance.getBoard());
-        // if( result != 0){
-        //     if(result == this.playerMagic){
-        //         // Finish game and present youwin screen
-        //     } else if (result == 3){
-        //         // Finish game and present draw screen
-        //     } else {
-        //         // Finish game and present lose screen
-        //     }
-        // }
-        played = true;
-    }  
+    // [Command]
+    // private void CmdCastMagic(int xpos, int ypos, int playerMagic){
+    //     OnlineOrquestrator.setPlay(xpos, ypos, playerMagic, true);
+    // }
+
+    // [ClientRpc]
+    // private void RpcUpdateBoard(int xpos, int ypos, int playerMagic){
+    //     if(!isLocalPlayer) return;
+    //     OnlineOrquestrator.setPlay(xpos, ypos, playerMagic, false);
+    //     OnlineOrquestrator.changeTurn(lastPlayed, false);
+    //     //BoardManager.instance.setMagic(i,j,playerMagic);
+    //     // int result = BoardManager.instance.isGameOver(BoardManager.instance.getBoard());
+    //     // if( result != 0){
+    //     //     if(result == this.playerMagic){
+    //     //         // Finish game and present youwin screen
+    //     //     } else if (result == 3){
+    //     //         // Finish game and present draw screen
+    //     //     } else {
+    //     //         // Finish game and present lose screen
+    //     //     }
+    //     // }
+    //     played = true;
+    // }  
 }
